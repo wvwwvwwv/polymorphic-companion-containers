5b62684a26385ee47bf4ae503442f30f82871cc2
Changgyoo Park <wvwwvwwv@me.com>
2025-05-24 18:29:42 +0200

    Impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,66 @@
-// #![deny(missing_docs, warnings, clippy::all, clippy::pedantic)]
+#![deny(
+    missing_docs,
+    warnings,
+    clippy::all,
+    clippy::missing_safety_doc,
+    clippy::pedantic
+)]
 #![doc = include_str!("../README.md")]
 
-use std::mem::{MaybeUninit, align_of};
+use std::mem::{MaybeUninit, align_of, forget, transmute};
+use std::ops::{Deref, DerefMut};
 use std::ptr::drop_in_place;
 
 const DEFAULT_STACK_SIZE: usize = 16384;
 
+/// A dynamically sized stack.
+#[derive(Debug)]
 pub struct DS<const SIZE: usize = DEFAULT_STACK_SIZE> {
     buffer: MaybeUninit<[u8; SIZE]>,
     cursor: usize,
 }
 
+/// Errors.
+#[derive(Debug)]
+pub enum Error<E> {
+    /// Failed to construct a value.
+    ConstructionFailed(E),
+    /// Stack is full.
+    Full,
+}
+
 impl<const SIZE: usize> DS<SIZE> {
+    /// Creates a new dynamically sized stack.
+    #[must_use]
     pub fn new() -> Self {
         Self {
             buffer: MaybeUninit::uninit(),
             cursor: 0,
         }
     }
-    pub fn alloc_sized<'s, T: Sized, C: FnOnce() -> T>(
-        &'s mut self,
+
+    /// Allocates a new value on the stack.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the stack is full or if the alignment of the type is not supported.
+    pub fn alloc_sized<E, T: Sized, C: FnOnce() -> Result<T, E>>(
+        &mut self,
         constructor: C,
-    ) -> Result<Handle<'s, T, SIZE>, ()> {
+    ) -> Result<Handle<T, SIZE>, Error<E>> {
         let alignment = align_of::<T>();
         let size = size_of::<T>();
         let buffer_start = unsafe { self.buffer.as_ptr().cast::<u8>().add(self.cursor) };
         let aligned_offset = buffer_start.align_offset(alignment);
-        if aligned_offset == usize::MAX {
-            return Err(());
-        } else if self
-            .cursor
-            .checked_add(aligned_offset)
-            .and_then(|start| start.checked_add(size))
-            .filter(|end| *end <= SIZE)
-            .is_none()
+        if aligned_offset == usize::MAX
+            || self
+                .cursor
+                .checked_add(aligned_offset)
+                .and_then(|start| start.checked_add(size))
+                .filter(|end| *end <= SIZE)
+                .is_none()
         {
-            return Err(());
+            return Err(Error::Full);
         }
         let ptr = unsafe {
             self.buffer
@@ -44,7 +69,7 @@ impl<const SIZE: usize> DS<SIZE> {
                 .add(self.cursor + aligned_offset)
                 .cast::<T>()
         };
-        unsafe { ptr.write(constructor()) };
+        unsafe { ptr.write(constructor().map_err(|e| Error::ConstructionFailed(e))?) };
 
         let old_cursor = self.cursor;
         self.cursor = self.cursor + aligned_offset + size;
@@ -62,12 +87,21 @@ impl<const SIZE: usize> DS<SIZE> {
         })
     }
 
+    /// Allocates a new value on the stack.
+    #[must_use]
     pub fn buffer_addr(&self) -> usize {
         self.buffer.as_ptr() as usize
     }
 }
 
-pub struct Handle<'s, T: ?Sized, const SIZE: usize> {
+impl<const SIZE: usize> Default for DS<SIZE> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+/// A dynamically sized stack handle.
+pub struct Handle<'s, T: ?Sized, const SIZE: usize = DEFAULT_STACK_SIZE> {
     inst: &'s mut T,
     addr: usize,
     old_cursor: usize,
@@ -76,19 +110,49 @@ pub struct Handle<'s, T: ?Sized, const SIZE: usize> {
 }
 
 impl<'s, T: ?Sized, const SIZE: usize> Handle<'s, T, SIZE> {
+    /// Allocates a new value on the stack.
     pub fn split(&mut self) -> (&mut T, &mut DS<SIZE>) {
         (self.inst, self.stack)
     }
-}
 
-impl<'s, T: ?Sized, const SIZE: usize> Drop for Handle<'s, T, SIZE> {
-    fn drop(&mut self) {
-        self.stack.cursor = self.old_cursor;
-        let destructor: fn(usize) = unsafe { std::mem::transmute::<usize, _>(self.destructor) };
-        destructor(self.addr);
+    /// Allocates a new value on the stack.
+    #[must_use]
+    pub fn convert<U>(self) -> Handle<'s, U, SIZE>
+    where
+        T: DerefMut<Target = U>,
+        U: ?Sized,
+    {
+        let converted: Handle<U, SIZE> = Handle {
+            inst: &mut **self.inst,
+            addr: self.addr,
+            old_cursor: self.old_cursor,
+            stack: self.stack,
+            destructor: self.destructor,
+        };
+        let transmuted: Handle<U, SIZE> = unsafe { transmute(converted) };
+        forget(self);
+        transmuted
     }
 }
 
+impl<T: ?Sized, const SIZE: usize> Deref for Handle<'_, T, SIZE> {
+    type Target = T;
+    fn deref(&self) -> &T {
+        self.inst
+    }
+}
+
+impl<T: ?Sized, const SIZE: usize> Drop for Handle<'_, T, SIZE> {
+    fn drop(&mut self) {
+        self.stack.cursor = self.old_cursor;
+        if self.destructor != 0 {
+            let destructor: fn(usize) = unsafe { transmute(self.destructor) };
+            destructor(self.addr);
+        }
+    }
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
@@ -97,9 +161,7 @@ mod tests {
         static mut NUM_DROPPED: usize = 0;
 
         #[derive(Debug)]
-        struct Data<const SIZE: usize> {
-            value: MaybeUninit<[u8; SIZE]>,
-        }
+        struct Data<const SIZE: usize>(MaybeUninit<[u8; SIZE]>);
 
         impl<const SIZE: usize> Drop for Data<SIZE> {
             fn drop(&mut self) {
@@ -109,26 +171,83 @@ mod tests {
 
         let mut ds = DS::<1024>::new();
         let mut handle1 = ds
-            .alloc_sized(|| Data::<512> {
-                value: MaybeUninit::uninit(),
-            })
+            .alloc_sized(|| Ok::<_, ()>(Data::<512>(MaybeUninit::uninit())))
             .unwrap();
-        let (data1, ds1) = handle1.split();
+        let (_, ds1) = handle1.split();
         let mut handle2 = ds1
-            .alloc_sized(|| Data::<400> {
-                value: MaybeUninit::uninit(),
-            })
+            .alloc_sized(|| Ok::<_, ()>(Data::<400>(MaybeUninit::uninit())))
             .unwrap();
-        let (data2, ds2) = handle2.split();
+        let (_, ds2) = handle2.split();
         assert!(
-            ds2.alloc_sized(|| Data::<400> {
-                value: MaybeUninit::uninit(),
-            })
-            .is_err()
+            ds2.alloc_sized(|| Ok::<_, ()>(Data::<400>(MaybeUninit::uninit())))
+                .is_err()
         );
         drop(handle2);
         drop(handle1);
         assert_eq!(unsafe { NUM_DROPPED }, 2);
         assert_eq!(ds.cursor, 0);
+    }
+
+    #[test]
+    fn test_deref() {
+        trait A {
+            fn a(&self) -> usize;
+        }
+
+        #[derive(Debug)]
+        struct Data1(usize);
+
+        impl DerefMut for Data1 {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                self
+            }
+        }
+
+        impl Deref for Data1 {
+            type Target = dyn A;
+
+            fn deref(&self) -> &Self::Target {
+                self
+            }
+        }
+
+        impl A for Data1 {
+            fn a(&self) -> usize {
+                self.0
+            }
+        }
+
+        #[derive(Debug)]
+        struct Data2(String);
+
+        impl A for Data2 {
+            fn a(&self) -> usize {
+                self.0.len()
+            }
+        }
+
+        impl Deref for Data2 {
+            type Target = dyn A;
+
+            fn deref(&self) -> &Self::Target {
+                self
+            }
+        }
+
+        impl DerefMut for Data2 {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                self
+            }
+        }
+
+        let mut ds = DS::<1024>::new();
+        let handle_dyn: Handle<dyn A, 1024> = if ds.buffer_addr() % 2 == 0 {
+            ds.alloc_sized(|| Ok::<_, ()>(Data1(11))).unwrap().convert()
+        } else {
+            ds.alloc_sized(|| Ok::<_, ()>(Data2("HELLO".to_owned())))
+                .unwrap()
+                .convert()
+        };
+        assert_eq!(handle_dyn.a(), 11);
     }
 }
