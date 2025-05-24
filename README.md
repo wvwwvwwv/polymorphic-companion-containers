0298e79ac350b11a11d792bd1324dd584e37f799
Changgyoo Park <wvwwvwwv@me.com>
2025-05-24 21:46:39 +0200

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ds
+# Companion Stack
+
+[![Cargo](https://img.shields.io/crates/v/scc)](https://crates.io/crates/scc)
+![Crates.io](https://img.shields.io/crates/l/scc)
+![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/wvwwvwwv/scalable-concurrent-containers/scc.yml?branch=main)
--- src/lib.rs
+++ src/lib.rs
@@ -110,44 +110,26 @@ pub struct Handle<'s, T: ?Sized, const SIZE: usize = DEFAULT_STACK_SIZE> {
 }
 
 impl<'s, T: ?Sized, const SIZE: usize> Handle<'s, T, SIZE> {
-    /// Allocates a new value on the stack.
+    /// Borrows itself as a mutable reference and the stack.
     pub fn split(&mut self) -> (&mut T, &mut DS<SIZE>) {
         (self.inst, self.stack)
     }
 
-    /// Allocates a new value on the stack.
+    /// Converts the handle to a different type.
     #[must_use]
-    pub fn into_future<U>(self) -> Handle<'s, dyn Future<Output = U>, SIZE>
+    pub fn into_deref_target<U>(self) -> Handle<'s, U, SIZE>
     where
-        T: Future<Output = U> + Sized,
+        T: DerefMut<Target = U>,
         U: ?Sized,
     {
-        let converted: Handle<dyn Future<Output = U>, SIZE> = Handle {
+        let converted: Handle<U, SIZE> = Handle {
             inst: self.inst,
             addr: self.addr,
             old_cursor: self.old_cursor,
             stack: self.stack,
             destructor: self.destructor,
         };
-        let transmuted: Handle<dyn Future<Output = U>, SIZE> = unsafe { transmute(converted) };
-        forget(self);
-        transmuted
-    }
-    /// Allocates a new value on the stack.
-    #[must_use]
-    pub fn convert<U>(self) -> Handle<'s, U, SIZE>
-    where
-        T: DerefMut<Target = U>,
-        U: ?Sized,
-    {
-        let converted: Handle<U, SIZE> = Handle {
-            inst: &mut **self.inst,
-            addr: self.addr,
-            old_cursor: self.old_cursor,
-            stack: self.stack,
-            destructor: self.destructor,
-        };
-        let transmuted: Handle<U, SIZE> = unsafe { transmute(converted) };
+        let transmuted: Handle<'s, U, SIZE> = unsafe { transmute(converted) };
         forget(self);
         transmuted
     }
@@ -173,6 +155,56 @@ impl<T: ?Sized, const SIZE: usize> Drop for Handle<'_, T, SIZE> {
             let destructor: fn(usize) = unsafe { transmute(self.destructor) };
             destructor(self.addr);
         }
+    }
+}
+
+impl<'s, T, U, const SIZE: usize> From<Handle<'s, T, SIZE>>
+    for Handle<'s, dyn DerefMut<Target = U>, SIZE>
+where
+    T: DerefMut<Target = U>,
+    U: ?Sized,
+{
+    fn from(value: Handle<'s, T, SIZE>) -> Self {
+        let converted: Handle<dyn DerefMut<Target = U>, SIZE> = Handle {
+            inst: value.inst,
+            addr: value.addr,
+            old_cursor: value.old_cursor,
+            stack: value.stack,
+            destructor: value.destructor,
+        };
+        let transmuted: Self = unsafe { transmute(converted) };
+        forget(value);
+        transmuted
+    }
+}
+
+impl<'s, U, const SIZE: usize> From<Handle<'s, dyn DerefMut<Target = U>, SIZE>>
+    for Handle<'s, U, SIZE>
+where
+    U: ?Sized,
+{
+    fn from(value: Handle<'s, dyn DerefMut<Target = U>, SIZE>) -> Self {
+        value.into_deref_target()
+    }
+}
+
+impl<'s, T, U, const SIZE: usize> From<Handle<'s, T, SIZE>>
+    for Handle<'s, dyn Future<Output = U>, SIZE>
+where
+    T: Future<Output = U>,
+    U: ?Sized,
+{
+    fn from(value: Handle<'s, T, SIZE>) -> Self {
+        let converted: Handle<dyn Future<Output = U>, SIZE> = Handle {
+            inst: value.inst,
+            addr: value.addr,
+            old_cursor: value.old_cursor,
+            stack: value.stack,
+            destructor: value.destructor,
+        };
+        let transmuted: Self = unsafe { transmute(converted) };
+        forget(value);
+        transmuted
     }
 }
 
@@ -236,7 +268,7 @@ mod tests {
                 })
             })
             .unwrap()
-            .into_future()
+            .into()
         } else {
             ds.alloc_sized(|| {
                 Ok::<_, ()>(async {
@@ -244,25 +276,21 @@ mod tests {
                 })
             })
             .unwrap()
-            .into_future()
+            .into()
         };
         drop(handle);
         assert_eq!(unsafe { NUM_DROPPED }, 1);
     }
     #[test]
     fn test_deref() {
+        static mut NUM_DROPPED: usize = 0;
+
         trait A {
             fn a(&self) -> usize;
         }
 
         #[derive(Debug)]
         struct Data1(usize);
-
-        impl DerefMut for Data1 {
-            fn deref_mut(&mut self) -> &mut Self::Target {
-                self
-            }
-        }
 
         impl Deref for Data1 {
             type Target = dyn A;
@@ -274,6 +302,18 @@ mod tests {
         impl A for Data1 {
             fn a(&self) -> usize {
                 self.0
+            }
+        }
+
+        impl DerefMut for Data1 {
+            fn deref_mut(&mut self) -> &mut Self::Target {
+                self
+            }
+        }
+
+        impl Drop for Data1 {
+            fn drop(&mut self) {
+                unsafe { NUM_DROPPED += 1 };
             }
         }
 
@@ -300,13 +340,32 @@ mod tests {
         }
 
         let mut ds = DS::<1024>::new();
+        let mut handle_deref_mut: Handle<dyn DerefMut<Target = dyn A>, 1024> =
+            if ds.buffer_addr() % 2 == 1 {
+                ds.alloc_sized(|| Ok::<_, ()>(Data1(11))).unwrap().into()
+            } else {
+                ds.alloc_sized(|| Ok::<_, ()>(Data2("HELLO".to_owned())))
+                    .unwrap()
+                    .into()
+            };
+        assert_eq!(handle_deref_mut.a(), 5);
+
+        let (_, ds) = handle_deref_mut.split();
+
         let handle_dyn: Handle<dyn A, 1024> = if ds.buffer_addr() % 2 == 0 {
-            ds.alloc_sized(|| Ok::<_, ()>(Data1(11))).unwrap().convert()
+            ds.alloc_sized(|| Ok::<_, ()>(Data1(11)))
+                .unwrap()
+                .into_deref_target()
         } else {
             ds.alloc_sized(|| Ok::<_, ()>(Data2("HELLO".to_owned())))
                 .unwrap()
-                .convert()
+                .into_deref_target()
         };
         assert_eq!(handle_dyn.a(), 11);
+
+        drop(handle_dyn);
+        drop(handle_deref_mut);
+
+        assert_eq!(unsafe { NUM_DROPPED }, 1);
     }
 }
